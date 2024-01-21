11a6cd2849fde0462c1680d5ae48f3b05c38bd56
朱淦New <350050183@qq.com>
2024-01-21 13:00:58 +0800

    Init commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-
+import { Button } from 'ant-design-vue';
 defineProps<{ msg: string }>()
 
 const count = ref(0)
@@ -8,7 +8,8 @@ const count = ref(0)
 
 <template>
   <h1>{{ msg }}</h1>
-ddddd
+  ddddd
+  <Button>ddddd</Button>
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
     <p>
@@ -16,11 +17,11 @@ ddddd
       <code>components/HelloWorld.vue</code> to test HMR
     </p>
   </div>
-asdfd
+  asdfd
   <p>
     Check out
     <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
+    >create-vue</a
     >, the official Vue + Vite starter
   </p>
   <p>
